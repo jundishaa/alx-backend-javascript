4bc4564347e7cccdda9294c1b2633bfe845e659d
jundi yusuf <jundiyusuf10@gmail.com>
2024-10-02 11:56:34 +0000

    2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,13 +1,9 @@
 export default function handleResponseFromAPI(promise) {
   return promise
-    .then(() => {
-      console.log('Got a response from the API');
-      return {
-        status: 200,
-        body: 'success',
-      };
-    })
-    .catch(() => {
-      return new Error();
-    });
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
+    .then(() => console.log('Got a response from the API'))
+    .catch(() => new Error());
 }
