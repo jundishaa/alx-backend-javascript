85e35600bc7e1e9f1fc30ca58b500824896f277f
jundi yusuf <jundiyusuf10@gmail.com>
2024-10-04 08:46:06 +0000

    9

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -2,12 +2,12 @@ export default function guardrail(mathFunction) {
   const queue = [];
 
   try {
-    const result = mathFunction();
-    queue.push(result);
-  } catch (error) {
-    queue.push(error.message);
+    queue.push(mathFunction());
+  } catch (err) {
+    queue.push(String(err));
   } finally {
     queue.push('Guardrail was processed');
   }
+
   return queue;
 }
