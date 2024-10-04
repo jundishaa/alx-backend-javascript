67ae7ab2a16e8c6b29a326564c99d727338d7d2c
jundi yusuf <jundiyusuf10@gmail.com>
2024-10-04 08:27:49 +0000

    3

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,43 +1,30 @@
 export default class Currency {
   constructor(code, name) {
-    this._code = code;
-    this._name = name;
-
-    this._validateCode(code);
-    this._validateName(name);
+    this.code = code;
+    this.name = name;
   }
 
   get code() {
     return this._code;
   }
 
+  set code(value) {
+    this._code = value;
+  }
+
   get name() {
     return this._name;
   }
 
-  set code(value) {
-    this._validateCode(value);
-    this._code = value;
-  }
-
   set name(value) {
-    this._validateName(value);
     this._name = value;
   }
 
+  /**
+   * Creates the full string representation of this Currency.
+   * @returns {String}
+   */
   displayFullCurrency() {
-    return `${this._name} (${this._code})`;
-  }
-
-  _validateCode(value) {
-    if (typeof value !== 'string') {
-      throw new TypeError('Code must be a string');
-    }
-  }
-
-  _validateName(value) {
-    if (typeof value !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
+    return `${this.name} (${this.code})`;
   }
 }
